7c079e2fd229952e439cae38298415775887e718
Mike <mvattuone@gmail.com>
2014-11-20 20:31:54 -0800

    remove cruft

--- paintedword/urls.py
+++ paintedword/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url (r'^ziplookup/',include('ziplookup.urls')),
 )
 
 urlpatterns += patterns('paintedword.views',
