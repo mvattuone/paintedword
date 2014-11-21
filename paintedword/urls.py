f91754349f120b0cc3442885eaa431d73351b15e
Mike <mvattuone@gmail.com>
2014-11-20 20:28:57 -0800

    restructure ziplookup so we only install one app

--- paintedword/urls.py
+++ paintedword/urls.py
@@ -14,7 +14,11 @@ urlpatterns += patterns('paintedword.views',
     (r'^(?P<slug>[\w-]+)/$', 'campaign_render'),
     (r'^(?P<slug>[\w-]+)/upload_raw_photo$', 'upload_raw_photo'), #make sure no trailing slash
     (r'^(?P<slug>[\w-]+)/submit$', 'submit')
+)
 
+#zip lookup for converting zip code to city on image.
+urlpatterns = patterns('paintedword.ziplookup',
+    (r'^zip_lookup/{0,1}$', 'zip_lookup'),
 )
 
 if settings.DEBUG:
--- paintedword/ziplookup.py
+++ paintedword/ziplookup.py
@@ -1,3 +1,14 @@
+from django.db import models
+from django.contrib.localflavor.us import models as us_models
+
+class ZipCode(models.Model):
+    zipcode = models.CharField(max_length=10,db_index=True)
+    city = models.CharField(max_length=255)
+    state = us_models.USPostalCodeField()
+    
+    def __unicode__(self):
+        return self.zipcode
+
 from django.http import HttpResponse,HttpResponseServerError
 from django.shortcuts import render_to_response
 from django.conf import settings
