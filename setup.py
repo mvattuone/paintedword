64d5db551dd954a5f5483f3121930e35a7c8685e
Mike <mvattuone@gmail.com>
2014-11-25 02:38:08 -0800

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='painted-word',
-    version='0.0.2',
+    version='0.0.3',
     author='Mike Vattuone',
     author_email='mvattuone@gmail.com',
     url='https://github.com/mvattuone/painted-word',
