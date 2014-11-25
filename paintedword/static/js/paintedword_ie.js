2c4354db88799f33b6289be632b1f01963bad534
Mike <mvattuone@gmail.com>
2014-11-25 02:44:16 -0800

    remove some code that was breaking collectstatic

--- paintedword/static/js/paintedword_ie.js
+++ paintedword/static/js/paintedword_ie.js
@@ -14,23 +14,23 @@ if ( !Array.prototype.forEach ) {
 }
       
 //upload raw file
-var uploader = new qq.FileUploader({
-	element: document.getElementById('file-uploader'),
-	action: 'upload_raw_photo',
-	onComplete: function(id, fileName, responseJSON) {
-		console.log(responseJSON);
+// var uploader = new qq.FileUploader({
+// 	element: document.getElementById('file-uploader'),
+// 	action: 'upload_raw_photo',
+// 	onComplete: function(id, fileName, responseJSON) {
+// 		console.log(responseJSON);
 
-        switchStep();
-        $('#id_raw_photo_pk').val(responseJSON.raw_photo_pk);
-        $('#id_raw_photo_url').val(responseJSON.file_url);
+//         switchStep();
+//         $('#id_raw_photo_pk').val(responseJSON.raw_photo_pk);
+//         $('#id_raw_photo_url').val(responseJSON.file_url);
 
-        //draw image to canvas
-        var canvas = document.getElementById("canvas");
-        var context = canvas.getContext("2d");
+//         //draw image to canvas
+//         var canvas = document.getElementById("canvas");
+//         var context = canvas.getContext("2d");
 
-        drawPhoto(context,responseJSON.file_url);
-	}
-});
+//         drawPhoto(context,responseJSON.file_url);
+// 	}
+// });
 
 //upload form
  
